1af4c7dcc359357f56debee6ed962ffcf0d1f34a
suraj Boddula <sriman.suraj1@gmail.com>
2021-04-13 13:32:51 +0000

    added powershell script

---
+++ install.ps1
@@ -0,0 +1,5 @@
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Install Dependency Softwares
+choco install kubernetes-cli azure-cli curl wget telnet -y
